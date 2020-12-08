79736dac83f1aabc5976b91601de59a3bd1f7c4d
Max <maxblanck@outlook.fr>
2020-12-08 10:56:29 +0100

    php Partie 1  Exercice 5

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 5 Partie 1</title>
+</head>
+<body>
+    <h1>Exercice 5 Partie 1</h1>
+
+    <?php
+    $int = NULL;
+    echo $int;
+    $int = 3;
+    echo $int;
+    ?>
+
+</body>
+</html>
